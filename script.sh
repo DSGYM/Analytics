4958bcda7879421370f60c0c6864709786d4e699
markus <langmarkus@hotmail.com>
2020-03-07 20:30:15 +0100

    bla

--- script.sh
+++ script.sh
@@ -28,8 +28,7 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 ### Docker Compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.26.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 else
   echo "Docker will not be installed"
